7b545a1411892231893f8d4b0944982d499faacf
Leana N <leananeparidze@gmail.com>
2020-05-30 18:31:08 -0700

    more style adjustments and addition of the cell age property

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -10,9 +10,9 @@ const Main = () => {
   const [grid, setGrid] = useState([]);
 
   const [selectedPreset, setSelectedPreset] = useState('');
-  const presets = [{name: 'Preset 1', func: preset1}, 
-                   {name: 'Preset 2', func: preset2},
-                   {name: 'Preset 3', func: preset3},
+  const presets = [{name: 'Spaceships', func: preset1}, 
+                   {name: 'Oscillators', func: preset2},
+                   {name: 'Glider Gun', func: preset3},
                    {name: 'Randomize', func: randomPreset}]
   const [reload, setReload] = useState(false);
 
@@ -29,12 +29,12 @@ const Main = () => {
       let arr = new Array(25);
       
       for(let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(55)
+        arr[i] = new Array(75)
       }
 
       for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr[i].length; j++) {
-          arr[i][j] = {alive: false, coor: [i,j]}
+          arr[i][j] = {alive: false, coor: [i,j], age: 1}
         }
       }
       setGrid(arr)
@@ -102,18 +102,18 @@ const Main = () => {
       <header>
         <h1>Game of Life</h1>
         <div style={{display: 'flex', alignItems: 'center'}}>
-          <div style={{fontSize: '16px'}}>rules:</div>
+          <div style={{fontSize: '16px'}}>rules</div>
           <img style={{width: '65px', height: '65px'}} src={img}/>
           <div className="rules">
-            <div>1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.</div>
-            <div>2. Any live cell with two or three live neighbours lives on to the next generation.</div>
-            <div>3. Any live cell with more than three live neighbours dies, as if by overpopulation.</div>
-            <div>4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</div>
+            <div>1. live cell with fewer than two live neighbours dies (underpopulation)</div>
+            <div>2. live cell with more than three live neighbours dies (overpopulation)</div>
+            <div>3. dead cell with exactly three live neighbours becomes a live cell (reproduction)</div>
+            <div>4. live cell with two or three live neighbours lives on to the next generation</div>
           </div>
         </div>
       </header>
 
-      <h2>_generation: # {generation}</h2>
+      <h2>__generation: # {generation}</h2>
 
       <Grid grid={grid} setGrid={setGrid} />
 
@@ -124,7 +124,7 @@ const Main = () => {
                     simulateRef.current = true;
                     startSimulation();
                     }}
-                  disabled={simulate ? true : false}>Play</button>
+                  disabled={simulate ? true : false} style={{border: '1px solid #CC00FF'}}>Play</button>
           <button onClick={pauseSimulation}>Pause</button>
           <button onClick={clearSimulation}>Clear</button>
         </div>
